891adc6c4a718b9db51bb6c5d07a83319bbcc7b0
rsneal01 <rsneal01@gmail.com>
2022-01-23 10:37:56 -0800

    commit tree 905bda8cbb6812637ec9efa3e589e5a5a55fa44f

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+
+    handleFocusEvent = () => console.log('Good!')
+    
+    handleBlurEvent = () => console.log('Hey! Eyes on me!')
+
+    render(){
+        return(
+                <button 
+                onFocus={this.handleFocusEvent}
+                onBlur={this.handleBlurEvent}
+                />
+        )
+    }
+}
+export default EyesOnMe;
